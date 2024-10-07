bef7258ba9420a5a35e8fed2fcca69c22a7b9391
valeriy korovin <1k9o9r5@gmail.com>
2024-10-08 02:45:47 +0300

    HW19.1_v1_'add initial variables'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 (function () {
-    
+    const apiKey = 'e7e3d66c2dafbe50adb8343082912ce9';
+    const lat = 46.29;
+    const lon = 30.43;
+    const weatherDiv = document.querySelector('#example');
+    const buttonRechange = document.createElement('button');
+    buttonRechange.classList.add('rechange');
+    buttonRechange.textContent = 'Rechange';
+    weatherDiv.insertAdjacentElement('afterend', buttonRechange);
+
 })()

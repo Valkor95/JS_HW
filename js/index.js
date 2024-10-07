89acf944c0ec907d6802055e83198fef094be6fe
valeriy korovin <1k9o9r5@gmail.com>
2024-10-08 02:28:52 +0300

    HW18.1_v5_'addition funcTimer + style'

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 (function () {
     let hours = 1;
     let minutes = 25;
+    let formatTime = '';
 
     const example = document.querySelector('#example')
 
     const timerUpdate = () => {
-        const formatTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
+        formatTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
         example.innerHTML = `
         <h1>${formatTime}</h1>
         `
@@ -19,6 +20,10 @@
         if(hours === 1 && minutes === 27){
             clearInterval(timer);
             example.classList.replace('start', 'finish');
+            example.innerHTML = `
+        <h1>FINISH! <br> ${formatTime}</h1>
+        `
+            console.log(formatTime)
         }
     }
 

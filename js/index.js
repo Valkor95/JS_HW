9278f80359db25af7ba84a075558379277da8c6c
valeriy korovin <1k9o9r5@gmail.com>
2024-10-08 01:47:24 +0300

    HW18.1_v1_'create hours/minutes/timerUpdate'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 (function () {
-    
+    const hours = 1;
+    const minutes = 25;
+
+    const example = document.querySelector('#example')
+
+    const timerUpdate = () => {
+        const formatTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
+        const title = example.innerHTML = `
+        <h1>${formatTime}<h1/>
+        `
+        title.classList.add('start')
+
+    }
 })()

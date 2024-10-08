302269876aa0a6290406fb061c27c1eca7250ce9
valeriy korovin <1k9o9r5@gmail.com>
2024-10-09 01:27:38 +0300

    HW19.1_v2_'add bootstrap'

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
 (function () {
     const apiKey = 'e7e3d66c2dafbe50adb8343082912ce9';
-    const lat = 46.29;
-    const lon = 30.43;
+    const lat = 46.40;
+    const lon = 30.50;
     const weatherDiv = document.querySelector('#example');
     const buttonRechange = document.createElement('button');
     buttonRechange.classList.add('rechange');
     buttonRechange.textContent = 'Rechange';
     weatherDiv.insertAdjacentElement('afterend', buttonRechange);
 
+    const getWeather = async () => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+            if (!response.ok) {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
+            const data = await response.json();
+
+            let weatherInfo = `<h1>Погода в ${data.name}</h1>`;
+            weatherInfo += `<p>Координаты: (${data.coord.lat}, ${data.coord.lon})</p>`;
+
+            weatherInfo += `<h2>Основные параметры:</h2>`;
+            for (const key in data.main) {
+                weatherInfo += `<p>${key}: ${data.main[key]}</p>`;
+            }
+
+            weatherInfo += `<h2>Дополнительные условия:</h2>`;
+            for (const condition of data.weather) {
+                weatherInfo += `<p>${condition.main}: ${condition.description}</p>`;
+            }
+
+            weatherDiv.innerHTML = weatherInfo;
+        } catch (error) {
+            weatherDiv.innerHTML = `<p>${error.message}</p>`;
+        }
+    };
+
+    getWeather()
+;
 })()

1bc265fc60195fd3b4b6bca4203236362448c6df
valeriy korovin <1k9o9r5@gmail.com>
2024-10-09 12:26:25 +0300

    HW18.1_v6_'fix and change logic'

--- js/index.js
+++ js/index.js
@@ -1,34 +1,58 @@
 (function () {
-    let hours = 1;
-    let minutes = 25;
-    let formatTime = '';
+    let hours = 0;
+    let minutes = 1;
+    let seconds = 25;
+    let hoursEnd = null;
+    let minutesEnd = null;
+    let secondsEnd = null;
+    let intervalId = null;
 
     const example = document.querySelector('#example')
+    const buttonTimer = document.createElement('button')
+    buttonTimer.textContent = 'Завести таймер';
 
     const timerUpdate = () => {
-        formatTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
+        const time = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
         example.innerHTML = `
-        <h1>${formatTime}</h1>
+        <h1>${time}</h1>
         `
-        example.classList.add('start')
     }
 
-    const funcTimer = () => {
-        minutes++;
-        timerUpdate();
+    const timer = () => {
+        hoursEnd = parseInt(prompt("Введіть години", "0"), 10);
+        minutesEnd = parseInt(prompt("Введіть хвилини", "0"), 10);
+        secondsEnd = parseInt(prompt("Введіть секунди", "0"), 10);
 
-        if(hours === 1 && minutes === 27){
-            clearInterval(timer);
-            example.classList.replace('start', 'finish');
-            example.innerHTML = `
-        <h1>FINISH! <br> ${formatTime}</h1>
-        `
-            console.log(formatTime)
-        }
     }
 
-    const timer = setInterval(funcTimer, 1000)
+    const startTimer = () => {
+        timer();
 
+        intervalId = setInterval(() => {
+            seconds++;
+            if (seconds === 60){
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes === 60) {
+                hours++
+                minutes = 0;
+            }
+            if (hours === 24) hours = 0;
+
+            timerUpdate();
+
+            if (hours > hoursEnd ||
+                (hours === hoursEnd && minutes > minutesEnd) ||
+                (hours === hoursEnd && minutes === minutesEnd && seconds >= secondsEnd)){
+                clearInterval(intervalId);
+                alert('Таймер завершив роботу!')
+            }
+        }, 1000)
+
+    };
+    buttonTimer.addEventListener('click', startTimer);
+    example.insertAdjacentElement('afterend', buttonTimer);
     timerUpdate()
 
 })()

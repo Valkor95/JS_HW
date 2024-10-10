59446d9a8203c12918d54019244c5d9a28623fa8
valeriy korovin <1k9o9r5@gmail.com>
2024-10-10 17:01:39 +0300

    HW3.3_v1_'2 version'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 (function () {
-    
+    const array = [1, 0, 3, 6, 9];
+
+    const value1 = array.join(' ')
+
+    console.log(value1)
+
+    // 2 variant
+
+    let value2 = '';
+
+    for (let i = 0; i < array.length; i++){
+        value2 += `${array[i]} `
+    }
+    console.log(value2)
 })()

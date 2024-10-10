b07e1b5a01d2052304bbd6d0fd05a9e455899dd0
valeriy korovin <1k9o9r5@gmail.com>
2024-10-10 16:35:57 +0300

    HW3.1_v1_'type of data'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 (function () {
-    
+    const type = [1, 'a', null, undefined, 134566n, true, Symbol('Hi'), {name: 'Johny'}, function(){}, [1, 2, 3]]
+
+    for (i = 0; i < type.length; i++){
+        console.log(typeof type[i])
+    }
 })()

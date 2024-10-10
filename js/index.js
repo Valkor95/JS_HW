a10a993d9942f027a5ae8c6fd1fa4d98d47dec07
valeriy korovin <1k9o9r5@gmail.com>
2024-10-10 16:41:07 +0300

    HW3.2_v1_'3 data from 3 users'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 (function () {
-    
+    const div = document.querySelector('#example')
+
+    const user1 = prompt('Information from user1');
+    const user2 = prompt('Information from user2')
+    const user3 = prompt('Information from user3')
+
+    div.innerHTML = `
+        <p>${user1}</p>
+        <p>${user2}</p>
+        <p>${user3}</p>
+    `
 })()

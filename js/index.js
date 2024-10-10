dac6552d5311714403bee230e8b5db2df98575b8
valeriy korovin <1k9o9r5@gmail.com>
2024-10-10 17:29:09 +0300

    HW4.2_v1_'add logic to find same/duplicates digit'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 (function () {
-    
+    const request = +prompt('Please enter a three digit number!')
+
+    if (request < 100 || request > 999){
+        alert('Invalid format, please enter a 3 digit number!');
+    }
+
+    const digits = String(request).split('');
+
+    const allSame = digits.every(digit => digit === digits[0]);
+
+    const duplicates = new Set (digits).size < digits.length;
+
+    console.log(`There are same digits? ${allSame ? 'Yes' : 'No'}`);
+    console.log(`There are duplicates? ${duplicates ? 'Yes' : 'No'}`)
+
 })()

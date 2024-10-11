56446a93c6fd9997e30c7b6623c46815e045c66e
valeriy korovin <1k9o9r5@gmail.com>
2024-10-11 16:01:02 +0300

    HW4.4_v1_'switch'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 (function () {
-    
+    let numOrStr = prompt('input number or string');
+
+    switch (true) {
+        case numOrStr === null:
+            console.log('You reject!');
+            break;
+        case numOrStr === '':
+            console.log('Empty string!');
+            break;
+        case /^[0-9]+$/.test(numOrStr):
+        case /^[a-zA-Z]+$/.test(numOrStr):
+            console.log('OK!');
+            break;
+        default:
+            console.log('Number is Ba_NaN')
+    }
 })()

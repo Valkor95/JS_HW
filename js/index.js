51fe6d1a800949325b7cff34147cf349eb2144bb
valeriy korovin <1k9o9r5@gmail.com>
2024-10-16 01:32:45 +0300

    HW5.4_v1_'add for and condition for search prime number'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 (function () {
-    
+    const num = +prompt('Enter integer!')
+
+    const isPrime = (number) => {
+        if (number <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(number); i++){
+            if (number % i === 0){
+                return false;
+            }
+        }
+        return true
+     }
+
+    console.log( isPrime(num) ? `${num} is prime number` : `${num} is not prime number`)
+
 })()

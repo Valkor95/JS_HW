53c9d787833b2411c79cfaf1a668368b0f3ba9ac
valeriy korovin <1k9o9r5@gmail.com>
2024-10-16 01:51:50 +0300

    HW6.1_v1_'add func for remove arguments'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 (function () {
-    
+    const filtrFunc = function (text, ...values) {
+        const argumentsData = Array.isArray(values[0]) ? values[0] : values;
+
+        const newText = text.split("").filter(item => !argumentsData.includes(item)).join("")
+        return newText;
+    }
+
+    console.log(filtrFunc( "Hello, World!", 'l', 'd'))
+    console.log(filtrFunc( "Hello, World!", ['l', 'd', 'o']))
 })()

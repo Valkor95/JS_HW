9931fe1edccbdc061d6086212cf7a2bd48395c81
valeriy korovin <1k9o9r5@gmail.com>
2024-10-16 01:21:07 +0300

    HW5.3_v1_'add for and pow condition'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 (function () {
-    
+    const value = +prompt('Enter integer!')
+
+    for (let i = 1; i <= 100; i++){
+        if(Math.pow(i, 2) > value){
+            break
+        }
+        console.log(i);
+    }
 })()

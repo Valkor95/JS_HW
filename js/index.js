e5d056ae58afaf51e48cf62e1c0a1ccbb643bb13
valeriy korovin <1k9o9r5@gmail.com>
2024-10-18 13:19:53 +0300

    HW23.1_v2_'add simple TODO/logic/style'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,40 @@
 (function () {
-    
+    class ToDoList{
+        constructor() {
+            this.todoInput = document.getElementById('todo-input');
+            this.addBtn = document.getElementById('add-btn');
+            this.todoList = document.getElementById('todo-list');
+
+            this.addTodo = this.addTodo.bind(this);
+            this.removeTodo = this.removeTodo.bind(this);
+
+            this.addBtn.addEventListener('click', this.addTodo);
+        }
+
+        addTodo(){
+            const taskText = this.todoInput.value;
+            if (taskText){
+                const listItem = document.createElement('li');
+                listItem.textContent = taskText;
+
+                const removeBtn = document.createElement('button');
+                removeBtn.textContent = 'Remove';
+                removeBtn.addEventListener('click', this.removeTodo);
+
+                listItem.append(removeBtn);
+                this.todoList.append(listItem);
+
+                this.todoInput.value = '';
+            }
+        }
+
+        removeTodo(event){
+            const listItem = event.target.parentElement;
+            this.todoList.removeChild(listItem);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        new ToDoList();
+    })
 })()

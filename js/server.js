9bbaf65a2a71629e9bde67114d993d5afb63a113
valeriy korovin <1k9o9r5@gmail.com>
2024-10-18 14:19:08 +0300

    HW23.1_v4_'add 2 version base config server with express'

--- js/server.js
+++ js/server.js
@@ -1,10 +1,34 @@
-const http = require("http");
-const PORT = 3000;
-const server = http.createServer(function (req, res) {
-    console.log("URL of your page:" + req.url);
-    res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-    res.end("It works fine");
-});
+// import http from 'http';
+//
+// const PORT = 3000;
+//
+// const server = http.createServer(function (req, res) {
+//     console.log("URL of your page:" + req.url);
+//     res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+//     res.end("It works fine");
+// });
+//
+// server.listen(PORT);
+// console.log("We are listening port" + PORT);
 
-server.listen(PORT);
-console.log("We are listening port" + PORT);
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+const users = [{id: 1, name: 'Mary'}, {id:2, name: 'Helen'}]
+app.listen(PORT, () => console.log("Server started"));
+
+app.get('/api/users/', function (req, res) {
+    res.send(JSON.stringify(users));
+})
+
+app.post('/api/users/', function (req, res) {
+    const newUser = req.body;
+    users.push(newUser);
+
+    res.send('Ok!')
+})

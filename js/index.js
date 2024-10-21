62fa2d012b3a80d2d8c20fe5d6ce5570eb4c1710
valeriy korovin <1k9o9r5@gmail.com>
2024-10-21 19:10:59 +0300

    HW7.1_v1_'add logic sum'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 (function () {
-    
+    const sumReduce = () => {
+        let valueItr = 0;
+
+        return function operationSum (value){
+            const result = value + valueItr;
+            valueItr += value;
+            return result;
+        }
+    }
+
+    const sum = sumReduce()
+
+    console.log(sum(4))
+    console.log(sum(6))
+    console.log(sum(10))
+    console.log(sum(7))
+
 })()

5c818e8e477a480ae154c329a183b3864f69a14a
valeriy korovin <1k9o9r5@gmail.com>
2024-10-23 00:35:11 +0300

    HW_7.2_v1 'add logic summary'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 (function () {
-    
+    function summary (a){
+        return function secondArgument(b){
+            return a + b;
+        }
+    }
+
+    console.log(summary(5)(2))
+    console.log(summary(2)(3))
+    console.log(summary(10)(89))
 })()

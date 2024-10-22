36291736ce6dd2ce40e8edb1efa17e1765434d13
valeriy korovin <1k9o9r5@gmail.com>
2024-10-23 01:03:49 +0300

    HW_7.3_v1 'add logic itteration'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 (function () {
-    
+    function itteration (){
+        let userNumber = null;
+        for (let i = 0; i <= 10; i++){
+            userNumber = prompt('Enter a number greater than one hundred!')
+
+            if (userNumber === null) {
+                alert("Input was cancelled.");
+                return;
+            }
+
+            const num = Number(userNumber);
+
+            if (!isNaN(num) && num > 100) {
+                alert(`You entered: ${userNumber}`);
+                return;
+            } else {
+                alert("Please enter a number greater than 100!");
+            }
+        }
+
+        alert(`The last number: ${userNumber}`);
+    }
+
+    itteration();
 })()
